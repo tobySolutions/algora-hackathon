a7bfc5cf90c6e138a7c8cc3753ffa509610c28d8
tdadadavid <davidtofunmidada@gmail.com>
2022-08-25 13:39:55 +0100

    add try/catch to handle errors

--- src/meeting/meeting.validation.js
+++ src/meeting/meeting.validation.js
@@ -1,14 +1,22 @@
 function validation(schema) {
   return async (req, res, next) => {
-    const { error } = await schema.validate(req.body);
-    if (error){
-      res.status(400).json({
-        status: 400,
-        error: error.message
+    try{
+      const {error} = await schema.validate(req.body);
+      if (error) {
+        res.status(400).json({
+          status: 400,
+          error: error.message
+        });
+      }
+
+      next();
+    }catch (err){
+      console.log(err);
+      res.status(500).json({
+        status: 500,
+        message: "An error occurred."
       });
     }
-
-    next();
   }
 
 }
