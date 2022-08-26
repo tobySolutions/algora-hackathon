33a4fa11c8235a5ef0d2dfe9064bd19f584f022e
tdadadavid <davidtofunmidada@gmail.com>
2022-08-26 19:37:19 +0100

    fix: changed port to dynamic port

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,7 @@ const configuration = {
   database: {
     MONGO_URI: process.env.MONGODB_URI
   },
+  PORT: process.env.PORT,
   APP_ID: process.env.APP_ID,
   APP_CERTIFICATE: process.env.APP_CERTIFICATE
 }
--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ const connectToDb = require('./config/database');
 const {database} = require('./config');
 const mongoose = require("mongoose");
 const app = require('./app');
+const config = require('./config')
 
 
 connectToDb(mongoose, database.MONGO_URI)
   .then(() => {
     console.log("Database connection established");
-    app.listen(3000, () => {
+    app.listen(config.PORT, () => {
       console.log(`server running on port 3000`);
     });
   })
